5a8f45d330bb5fd9c0ca8be147a92916a2f33610
Neal Miran <neal.miran@gmail.com>
2019-04-26 21:53:24 -0400

    Sort followers in descedning order, added loading spinner

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
     super(props);
     this.state = {
       query : '',
+      loading : false,
       playlists : []
     }
     this.search = this.search.bind(this);
@@ -19,9 +20,8 @@ class App extends Component {
     const BASE_URL = 'https://api.spotify.com/v1/search?';
     let FETCH_URL = BASE_URL + 'q=' + encodeURIComponent(this.state.query) + '&type=playlist&limit=50';
     let PLAYLIST_URL = 'https://api.spotify.com/v1/playlists/'
-    //const ALBUM_URL = 'https://api.spotify.com/v1/artists/';
-    const auth_token = 'Bearer BQBZvDMtCFDT3Mmk3ob9BkrQqwqsfigXEMmVPFwI6BJnktynaPkFwZTHCTcBZtTAHpcldv02Z6KDdMlwwEY5XsTTl-oETsc5JnjwhJb_AI53HrChRiTgG3W4QL2am-dF2iyaKf5SI5Z2eUmv';
-
+    const auth_token = 'Bearer BQBnVyReZ_B7L1LrOsjFm9Lob2HHsIx72IA9EmSJV91_2Me_2Q_fbAbnXDfL9gw-R1xk0CbO5n6FuF8tMApwwVvArW_B6jFQbLaSnilpERY_zIgIL0DQbWgstnL-DAd_x_1m8ChvcRV_B3FY';
+    
     let playlist = [];
 
     let response = await axios.get( FETCH_URL, {
@@ -32,42 +32,60 @@ class App extends Component {
       mode: 'cors',
       cache: 'default'
     })
-    let data = await response.data.playlists.items;
-    for(let i= 0; i < data.length; i++){
-      
-      let findPlaylist = await axios.get ( PLAYLIST_URL + data[i].id, {
-        headers : {
-          'Content-Type': 'application/json',
-          'Authorization': auth_token,
-        },
-        mode: 'cors',
-        cache: 'default'
-      })
-
-      playlist.push( {
-            id: data[i].id,
-            uri: data[i].uri,
-            imgUrl: data[i].images[0].url,
-            playListName: data[i].name,
-            owner: data[i].owner.display_name,
-            totalTracks: data[i].tracks.total,
-            followers: findPlaylist.data.followers.total
-          } );
-    }
-    this.setState( { playlists: playlist } )
     
-    console.log(data);
-    console.log(this.state.playlists[0].uri);
+    let data = await response.data.playlists.items;
+
+    console.log(response);
+
+    for(let i= 0; i < data.length; i++){
+
+      if (data[i].images[0]) {
+
+        let findPlaylist = await axios.get ( PLAYLIST_URL + data[i].id, {
+          headers : {
+            'Content-Type': 'application/json',
+            'Authorization': auth_token,
+          },
+          mode: 'cors',
+          cache: 'default'
+        })
+
+        playlist.push( {
+          id: data[i].id,
+          uri: data[i].uri,
+          link: data[i].external_urls.spotify,
+          imgUrl: data[i].images[0].url,
+          playListName: data[i].name,
+          owner: data[i].owner.display_name,
+          followers: findPlaylist.data.followers.total,
+          totalTracks: data[i].tracks.total
+        });
+      }
+    }    
+    this.setState( { 
+      loading: false,
+      playlists: playlist 
+    } )    
   };
 
   render() {
+    if(this.state.loading) {
+      return (
+        <div className="App-spinner">
+          <h1>Loading...</h1>
+          <i className="fas fa-8x fa-spinner fa-spin"></i>
+        </div>
+      )
+    }
+    let sortedPlaylists = this.state.playlists.sort((a,b) => a.followers - b.followers).reverse();
+    console.log(sortedPlaylists);
     return (
       <div className="App">
         <FormGroup>
           <InputGroup>
             <FormControl
                 type="text"
-                placeholder="Search Playlist"
+                placeholder="Playlist Search"
                 value = { this.state.query }
                 onChange= { event => { 
                   this.setState( { query: event.target.value } ) 
@@ -75,7 +93,7 @@ class App extends Component {
                 }
                 onKeyPress= { event=> {
                   if( event.key==='Enter' )
-                    this.search();
+                    this.setState( { loading : true }, this.search);
                 }}
             />
           </InputGroup>
@@ -86,6 +104,7 @@ class App extends Component {
                 key={p.id}
                 name={p.playListName}
                 img={p.imgUrl}
+                link={p.link}
                 owner={p.owner}
                 tracks={p.totalTracks}
                 followers={p.followers}
--- src/Playlist.js
+++ src/Playlist.js
@@ -6,7 +6,9 @@ class Playlist extends Component {
         return (
             <div className="Playlist">
                 <h1>Playlist Name: {this.props.name}</h1>
-                <img className="Playlist-img" src={this.props.img} alt="" />
+                <a href={this.props.link} rel="noopener noreferrer" target="_blank">
+                    <img className="Playlist-img" src={this.props.img} alt="" />
+                </a>
                 <h2>Owner: {this.props.owner}</h2>
                 <h2>Number of Tracks: {this.props.tracks}</h2>
                 <h2>Number of Followers: {this.props.followers}</h2>
