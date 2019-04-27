b8443abe91686477939324e22287712f384caf00
Neal Miran <neal.miran@gmail.com>
2019-04-27 01:54:15 -0400

    Query result in table format, CSS formating

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {FormGroup, FormControl, InputGroup } from 'react-bootstrap';
+import {FormGroup, FormControl, InputGroup, Container, Row, Col} from 'react-bootstrap';
 import './App.css';
 import axios from 'axios';
 import Playlist from './Playlist';
@@ -20,7 +20,7 @@ class App extends Component {
     const BASE_URL = 'https://api.spotify.com/v1/search?';
     let FETCH_URL = BASE_URL + 'q=' + encodeURIComponent(this.state.query) + '&type=playlist&limit=50';
     let PLAYLIST_URL = 'https://api.spotify.com/v1/playlists/'
-    const auth_token = 'Bearer BQBnVyReZ_B7L1LrOsjFm9Lob2HHsIx72IA9EmSJV91_2Me_2Q_fbAbnXDfL9gw-R1xk0CbO5n6FuF8tMApwwVvArW_B6jFQbLaSnilpERY_zIgIL0DQbWgstnL-DAd_x_1m8ChvcRV_B3FY';
+    const auth_token = 'Bearer BQBGoXeOhFS9YF2-PvbYHbfCuHWAlCG2xq79N-O9UHKN2vF0wn2aNW6itNwL6WcWPp-Ez_wJRTBoZyEAG9Rt_qaVJdFJGgnHhwPG-uv516xPNnKck4GMfhQh40_xzZx6DUQtVyJC8sAjGZp9';
     
     let playlist = [];
 
@@ -33,14 +33,16 @@ class App extends Component {
       cache: 'default'
     })
     
+    //Get the simplified playlist objects
     let data = await response.data.playlists.items;
 
-    console.log(response);
-
+    //Loop through the data array containing the responses
     for(let i= 0; i < data.length; i++){
 
+      //Skip playlists that have no Playlist image
       if (data[i].images[0]) {
 
+        //Get the full playlist object for each playlist result to get the followers
         let findPlaylist = await axios.get ( PLAYLIST_URL + data[i].id, {
           headers : {
             'Content-Type': 'application/json',
@@ -50,6 +52,7 @@ class App extends Component {
           cache: 'default'
         })
 
+        //Collect the needed items and push them in an empty array
         playlist.push( {
           id: data[i].id,
           uri: data[i].uri,
@@ -57,11 +60,14 @@ class App extends Component {
           imgUrl: data[i].images[0].url,
           playListName: data[i].name,
           owner: data[i].owner.display_name,
+          description: findPlaylist.data.description,
           followers: findPlaylist.data.followers.total,
           totalTracks: data[i].tracks.total
         });
       }
-    }    
+    }
+
+    //Update the loading boolean and playlist object state
     this.setState( { 
       loading: false,
       playlists: playlist 
@@ -69,6 +75,8 @@ class App extends Component {
   };
 
   render() {
+
+    //Add loading spinner (short circuit the render function)
     if(this.state.loading) {
       return (
         <div className="App-spinner">
@@ -77,12 +85,14 @@ class App extends Component {
         </div>
       )
     }
-    let sortedPlaylists = this.state.playlists.sort((a,b) => a.followers - b.followers).reverse();
+    
+    //Sort Playlist result by followers in decsending order
+    let sortedPlaylists = this.state.playlists.sort( ( a,b ) => a.followers - b.followers ).reverse();
     console.log(sortedPlaylists);
     return (
       <div className="App">
-        <FormGroup>
-          <InputGroup>
+        <FormGroup className="App-form">
+          <InputGroup size="lg">
             <FormControl
                 type="text"
                 placeholder="Playlist Search"
@@ -98,8 +108,30 @@ class App extends Component {
             />
           </InputGroup>
         </FormGroup>
-        <div>
-          {this.state.playlists.map( p => (
+        <div className="App-list">
+          {/* <Container className="Playlist-Title" fluid="true">
+            <Row className="Playlist-rowTitle">
+                <Col className="Playlist-colTitle">                
+                  Playlist Image
+                </Col>
+                <Col className="Playlist-colTitle">
+                    Playlist Name
+                </Col>
+                <Col className="Playlist-colTitle">
+                    Description
+                </Col>
+                <Col className="Playlist-colTitle">
+                    Owner
+                </Col>
+                <Col className="Playlist-colTitle">
+                    Number of Tracks
+                </Col>
+                <Col className="Playlist-colTitle">
+                    Number of Followers
+                </Col>
+            </Row>
+          </Container> */}
+          { sortedPlaylists.map( p => (
               <Playlist
                 key={p.id}
                 name={p.playListName}
@@ -108,6 +140,7 @@ class App extends Component {
                 owner={p.owner}
                 tracks={p.totalTracks}
                 followers={p.followers}
+                description={p.description}
               />
             )
           )}
--- src/Playlist.js
+++ src/Playlist.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
 import './Playlist.css';
+import { Container, Row, Col } from 'react-bootstrap';
 
 class Playlist extends Component {
+
     render() {
         return (
-            <div className="Playlist">
-                <h1>Playlist Name: {this.props.name}</h1>
-                <a href={this.props.link} rel="noopener noreferrer" target="_blank">
-                    <img className="Playlist-img" src={this.props.img} alt="" />
-                </a>
-                <h2>Owner: {this.props.owner}</h2>
-                <h2>Number of Tracks: {this.props.tracks}</h2>
-                <h2>Number of Followers: {this.props.followers}</h2>
-            </div>
+                <Container className="Playlist" fluid="true">
+                    <Row className="Playlist-row">
+                        <Col className="Playlist-col">                
+                            <a href={this.props.link} rel="noopener noreferrer" target="_blank">
+                                <img className="Playlist-img" src={this.props.img} alt="" />
+                            </a>
+                        </Col>
+                        <Col className="Playlist-col">
+                            Playlist Name: {this.props.name}
+                        </Col>
+                        <Col className="Playlist-col">
+                            Description: {this.props.description}
+                        </Col>
+                        <Col className="Playlist-col">
+                            Owner: {this.props.owner}
+                        </Col>
+                        <Col className="Playlist-col">
+                            Number of Tracks: {this.props.tracks}
+                        </Col>
+                        <Col className="Playlist-col">
+                            Number of Followers: {this.props.followers}
+                        </Col>
+                    </Row>
+                </Container>
         )
     }
 }
